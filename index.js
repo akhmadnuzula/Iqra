31f76bbf701e5f390041b5a776dec154af5f91d4
Akhmad Nuzula <akhmadnuzula@gmail.com>
2023-05-24 09:13:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import codePush from 'react-native-code-push';
 
-const codePushOptions = {
-  checkFrequency: codePush.CheckFrequency.ON_APP_START,
-  installMode: codePush.InstallMode.IMMEDIATE,
-};
-
-const CodePushedApp = codePush(codePushOptions)(App);
-
-AppRegistry.registerComponent(appName, () => CodePushedApp);
+AppRegistry.registerComponent(appName, () => App);
